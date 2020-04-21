75878b6937e279542ba88b938844850680cc0670
AkselDjun <akseldjun@gmail.com>
2020-04-21 13:48:36 +0300

    update

---
+++ Basic-Javascript Course_2/События и их обработчики/script.js
@@ -0,0 +1,37 @@
+let btn = document.querySelectorAll("button");
+let wrap = document.querySelector(".wrapper");
+let link = document.querySelector("a");
+
+// btn[0].onclick = function () {
+//     alert("You click first button!");
+// };
+
+// btn[0].onclick = function () {
+//     alert("You again click first button!");
+// };
+
+// btn[0].addEventListener("click", function (event) {
+// console.log(event);
+// let target = event.target;
+// target.style.display = "none";
+//     console.log("Произошло событие: " + event.type + " на элементе " + event.target);
+// });
+
+// wrap.addEventListener("click", function () {
+//     console.log("Произошло событие: " + event.type + " на элементе " + event.target);
+// });
+
+link.addEventListener("click", function (event) {
+    event.preventDefault();
+    console.log("Произошло событие: " + event.type + " на элементе " + event.target);
+});
+
+btn.forEach(function (item) {
+    item.addEventListener("mouseleave", function () {
+        console.log("Hello!");
+    });
+});
+
+// btn[0].addEventListener("mouseenter", function () {
+//     alert("You focus in first button");
+// });
