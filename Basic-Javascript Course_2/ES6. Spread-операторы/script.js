1e072bfcb990db1317bbef1264a4e32df76811dc
AkselDjun <akseldjun@gmail.com>
2020-04-22 14:16:28 +0300

    new info

---
+++ Basic-Javascript Course_2/ES6. Spread-операторы/script.js
@@ -0,0 +1,5 @@
+let video = ["youtube", "vimeo", "rutube"],
+    blogs = ["wordpress", "livejournal", "blogger"],
+    internet = [...video, ...blogs, "vk", "facebook"];//Оператор разворота
+
+console.log(internet);
---
+++ Basic-Javascript Course_2/Стандарт ES6. Интерполяция/script.js
@@ -0,0 +1,8 @@
+let name = "Alexei";
+let age = 20;
+let mail = "example@mail.ru";
+
+//document.write("Пользователю " + name + " " + age + " лет. Его почтовый адрес: " + mail);
+
+document.write(`Пользователю ${name} ${age} лет. Его почтовый адрес: ${mail}`);
+
