18cdbd17e8c94f1d61ce22a3ae7b6ce61fd270fe
AkselDjun <akseldjun@gmail.com>
2020-05-26 12:21:39 +0300

    final course-2

---
+++ Javascript-Course_2/Пишем парсер на JavaScript/parser.js
@@ -0,0 +1,16 @@
+window.addEventListener("DOMContentLoaded", () => {
+    const body = document.querySelector("body");
+
+    function recursy(element) {
+        element.childNodes.forEach(node => {
+            if (node.nodeName === "#text") {
+                return;
+            } else {
+                console.log(node);
+                recursy(node);
+            }
+        });
+    }
+
+    recursy(body);
+});
